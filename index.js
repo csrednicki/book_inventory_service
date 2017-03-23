ab29fd02e2b79636e188e619be4e3451d6d5972f
Cezary Średnicki <cezary.srednicki@agora.pl>
2017-03-23 11:43:43 +0100

    obsluga bledow

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ var app = express();
 function logRequest(req, res, next) {
     console.log('incoming request at ', new Date());
     next();
-};
-
+}
 
 
 function auth(req, res, next) {
@@ -13,13 +12,33 @@ function auth(req, res, next) {
     next();
 }
 
-//app.use(logRequest);
-//app.use(auth);
+app.use(logRequest);
+app.use(auth);
 
-app.get('/', [logRequest, auth], function (req, res) {
+app.get('/', function (req, res) {
     res.send('Hello World!');
 });
+
+app.get('/error', function(req, res) {
+    throw new Error('forced error');
+});
+
+app.use(clientError);
+app.use(serverError);
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+function clientError(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+}
+
+function serverError(err, req, res, next) {
+    var status = err.status || 500;
+    res.status(status);
+    console.error(err.stack);
+    res.send('Oh no: '+ status);
+}
