50b1531900d98684e4b3a70272274cf1ad6853ea
Cezary Średnicki <cezary.srednicki@agora.pl>
2017-03-23 13:13:03 +0100

    exports sum

--- test/test.js
+++ test/test.js
@@ -1,7 +1,8 @@
 var assert = require('assert');
+var sum = require('./sum');
 
 describe('Math in JS', function () {
     it('should support addition', function () {
-        assert.equal(1 + 1, 2);
+        assert.equal(sum(1,1), 2);
     });
 });
